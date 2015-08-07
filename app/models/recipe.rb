676d3006a1614f7aa1096ef1968ad3619b32b148
redpapercrane <redpapercrane@hotmail.com>
2015-08-07 14:46:45 +1000

    ingredients and directions finished, minor style change

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -10,8 +10,8 @@ class Recipe < ActiveRecord::Base
                                 reject_if: proc { |attributes| attributes['step'].blank? },
                                 allow_destroy: true
 
-  validates :title, :description, :image, presence: true
-  has_attached_file :image, styles: { :medium => "300x300#" }
+  validates :title, :description, presence: true
+  has_attached_file :image, styles: { :medium => "400x400#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
